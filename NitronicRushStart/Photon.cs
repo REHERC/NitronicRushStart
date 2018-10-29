d8efc62ccc8fa2716eb8ea2600585d26dbabb372
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-10-29 17:23:41 +0100

    Title bug "fixed"

--- NitronicRushStart/Photon.cs
+++ NitronicRushStart/Photon.cs
@@ -76,15 +76,18 @@ namespace NitronicRushStart
 
         public void Update()
         {
+            Scene scene = SceneManager.GetActiveScene();
             GameObject HUD = GameObject.Find("NitronicCountdownHUD");
 
-            Console.Title = Timex.ModeTime_.ToString();
-               
+            if (scene.name != "MainMenu")
+            {
+                Console.Title = Timex.ModeTime_.ToString();
+            }
+
             if (HUD != null)
             {
                 CountdownManager manager = HUD.GetComponent<CountdownManager>();
                 
-                Scene scene = SceneManager.GetActiveScene();
                 if (scene.name != "MainMenu" && scene.name != "LevelEditor" && G.Sys.GameManager_.ModeID_ != GameModeID.Adventure && G.Sys.GameManager_.ModeID_ != GameModeID.LostToEchoes)
                 {
                     manager.Time = Convert.ToSingle(Timex.ModeTime_);
